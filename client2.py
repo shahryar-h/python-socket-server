cd0aa00719a842c9dbb7b1c50d648d661f0d2493
advanceP <shahryar.haghighi.fard@gmail.com>
2020-05-23 09:05:03 -0400

    project finished need refactoring

--- client2.py
+++ client2.py
@@ -1,7 +1,7 @@
 import socket
 
 
-
+# Client configurations
 HEADER = 1024
 PORT = 9994
 SERVER = socket.gethostbyname(socket.gethostname())
@@ -13,6 +13,10 @@ ADDR = (SERVER,PORT)
 client = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
 client.connect(ADDR) 
 
+
+
+
+# sends and recives message to/from Server
 def send(msg):
     message = msg.encode(FORMAT)
     msg_length = len(message)
@@ -22,63 +26,89 @@ def send(msg):
     client.send(message)
     resp= client.recv(HEADER).decode(FORMAT)
     result = client.recv(HEADER).decode(FORMAT)
-    print('server wrote {}'.format(resp))
+    print("##############################################################################")
+    print(r"                               server's Response: ")
+    print("##############################################################################")
     print(result)
+    print("##############################################################################")
     
-    
+# prints clients menu
+# it reprint every time a request served so there is no need for client to scroll up for options 
 def printMenu():
     menuItems = ["1. Find customer", "2. Add customer", "3. Delete customer", "4. Update customer age", "5. Update customer address", "6. Update customer phone", "7. Print report", "8. Exit"]
-    print("Python DB Menu")
+    print("\n\n")
+    print("DB Menu")
     print(*menuItems , sep= '\n')
     
-printMenu()
 
-# send(message)
-# print("1) Create <name> \n 2) Read <name> \n 3) Update <name> \n 4) delete <name> \n 5) Print state \n 6) Exit server")
-# while True:
-    
-    
-#         try:
-#             message = int(input('Select:'))
-#             break
-#         except:
-#             print("That's not a valid option!")
-#     m = str(message)
-#     send(m)
+# keeps getting the requests from client until they close the connection by selecting 8
+# it only accepts values 1-8 and dose error handling in case of invalid input
 
-#     if m[0] == DISCONNECT_MESSAGE:
-#         break
-# while True:
-
-    # try:
-    #     # 1|asdsda|1211
-        
-    #     message = int(input('Select:'))
-    #     if message in range(1,9):
-    #         validatedMessage = str(message)
-    #         send(validatedMessage)
-    #     else:
-    #         print('Invalid command. Command must be in range 1 to 8')
-    #     if validatedMessage == DISCONNECT_MESSAGE:
-    #         break
-    #     printMenu()
-    # except:
-    #         print("That's not a valid option! Please enter command in range 1 to 8")\
 while True:
+    printMenu()
     message = input('Select:')
     if message.isnumeric() and int(message) in range(1,9):
         if int(message) == 1: 
             name = input('please enter name:')
             mes = message +"|"+ name
             send(mes)
-        
+        elif int(message) == 2:
+            newCustomer = ""        
+            thename = input('please name:')
+            if thename:
+                newCustomer = newCustomer + thename
+                age = input('please enter age:')
+                if age:
+                    newCustomer = newCustomer + "|" + age
+                
+                address = input('please enter address:')
+                if address:
+                    newCustomer = newCustomer + "|" + address
 
-        if int(message) == 5: 
+                phoneNumber = input('please enter phone number:')
+                if phoneNumber:
+                    newCustomer = newCustomer + "|" + phoneNumber
+
+                mes = message +"|"+ newCustomer
+                send(mes)
+            else:
+                print("can't add a customer without name")
+        elif int(message) == 3:
+                customerDelete = input('please Provide the name of the customer you want to delete:')
+                mes = message +"|"+ customerDelete
+                send(mes)
+        elif int(message) == 4:
+                updateCustomer = input('please the name of the customer you want to update name:')
+                if updateCustomer:
+                    customerAge = input('please the provide the age: ')
+                    mes = message + "|" + updateCustomer + "|" + customerAge
+                    send(mes)
+                else:
+                    print("can't Update a customer without name")
+        elif int(message) == 5:
+                updateCustomer = input('please the name of the customer you want to update Address: ')
+                if updateCustomer:
+                    customerAddress = input('please the provide the Address: ')
+                    mes = message + "|" + updateCustomer + "|" + customerAddress
+                    send(mes)
+                else:
+                    print("can't Update a customer without name")
+        elif int(message) == 6:
+                updateCustomer = input('please the name of the customer you want to update name:')
+                if updateCustomer:
+                    customerAge = input('please the provide the age: ')
+                    mes = message + "|" + updateCustomer + "|" + customerAge
+                    send(mes)
+                else:
+                    print("can't Update a customer without name")
+
+        if int(message) == 7: 
             send(message)
     else:
-        print('Invalid command. Command must be in range 1 to 8')
+        print('Invalid menu option. you must select a number in range 1 to 8')
  
     if message == DISCONNECT_MESSAGE:
+        send(message)
         break
 
 # send(DISCONNECT_MESSAGE)
--- server2.py
+++ server2.py
@@ -15,7 +15,12 @@ dataBase = {}
 server = socket.socket(socket.AF_INET,socket.SOCK_STREAM) 
 server.bind(ADDR)
 
-
+# loads the data to our server program
+# dose the following simple validations:
+    # makes sure that the data element has name or it ignore the record and print it
+    # clear up spaces
+    # and put "Not Available" as a place holder for the records that are missing(like a phone number) 
+# eventually we add all data to a dictionary in which the key is customer name and the value is a tuple of record
 def readDB():
     f = open("data.txt", "r")
     print(" Reading Data Base")
@@ -24,11 +29,20 @@ def readDB():
         if not lineItems[0]:
             print("line \n {} \n ignored".format(line))
             continue
+
+        placeHolder = 'Not Available'
+        
         t = ()
         for item in lineItems:
             item = item.strip()
-            t = t + (item,)
-        dataBase[t[0]] = t
+            item = item.rstrip('\n')
+            print(item)
+        for i in lineItems:
+                if not i.replace(" ", ""):
+                    t = t + (placeHolder,)
+                else:
+                    t = t + (i,)
+        dataBase[t[0]] = t[1:]
     print(" DataBase read Successfully!!")
     print("{} Valid record read from Database".format(len(dataBase)))
     
@@ -52,25 +66,90 @@ def findCustomer(name):
  
     if name[0] in dataBase.keys(): 
         tup = dataBase[name[0]]
+        print(tup)
         return True, '|'.join(tup)
     else: 
         return True, "Customer Not Found"
 
-def read(name):
-    print('reading {}'.format(name))
-    return True
+def addCustomer(name):
+    print('Customer {} added.'.format(name[0]))
 
-def update(name):
-    print('updating {}'.format(name))
-    return True
+    if name[0] in dataBase:
+        return True , '{} already exists in database'.format(name[0])
+    else:
+        t =()
+        placeHolder = 'not available'
+        for i in name[1:]:
+            if not i:
+                t = t + (placeHolder,)
+            else:
+                t = t + (i,)
+    
+        dataBase[name[0]] = t
+        return True , 'Customer {} added.'.format(name[0])
 
-def delete(name):
-    print('deleting {}'.format(name))
-    return True
+
+def deleteCustomer(name):
+    # return True , '{} Deleted!.'.format(name[0])
+
+    if name[0] in dataBase:
+        del dataBase[name[0]]
+        return True , '{} Deleted!.'.format(name[0])
+    else:
+        return True , '{} dose not exist in database. wanna add? select option1!.'.format(name[0])
+ 
+    
+
+def UpdateAge(name):
+    #print('deleting {}'.format(name))
+    if name[0] in dataBase:
+        customerRecord = dataBase[name[0]]
+        lst = list(customerRecord)
+        lst[0] = name[1]
+        t = tuple(lst)
+        dataBase[name[0]] = t
+        return True , ' age updated for {} '.format(name[0])
+    else:
+        return True, '{} dose not exist in our dataBase '.format(name[0])
+
+def UpdateAddress(name):
+    
+    if name[0] in dataBase:
+        customerRecord = dataBase[name[0]]
+        lst = list(customerRecord)
+        lst[1] = name[1]
+        t = tuple(lst)
+        dataBase[name[0]] = t
+        return True , ' Address updated for {} '.format(name[0])
+    else:
+        return True, '{} dose not exist in our dataBase '.format(name[0])
+
+
+def UpdateNumber(name):
+    #print('deleting {}'.format(name))
+    if name[0] in dataBase:
+        customerRecord = dataBase[name[0]]
+        lst = list(customerRecord)
+        lst[2] = name[1]
+        t = tuple(lst)
+        dataBase[name[0]] = t
+        print(dataBase[name[0]])
+        return True , ' Number updated for {} '.format(name[0])
+    else:
+        return True, '{} dose not exist in our dataBase '.format(name[0])
+
+
 
 def printState():
     print('printing state')
-    return True,str(dataBase)
+    resultToServer = ""
+
+    for customer in dataBase:
+        customerInformationTuple  = dataBase[customer]
+        customerInformationString = '|'.join(customerInformationTuple)
+        resultToServer = resultToServer + customerInformationString
+        
+    return True,str(resultToServer)
 
 def exitServer():
     print('exiting server')
@@ -78,18 +157,19 @@ def exitServer():
     
 OPTIONS = {
     1:findCustomer,
-    2:read,
-    3:update,
-    4:delete,
-    5:printState,
+    2:addCustomer,
+    3:deleteCustomer,
+    4:UpdateAge,
+    5:UpdateAddress,
+    6:UpdateNumber,
+    7:printState,
     8:exitServer
-
 }
 def handle_client(conn,addr):
     print("NEW CONNECTION {}".format(addr))
 
     connected = True
-    readDB()
+    
     while connected:
         msg_length = conn.recv(HEADER).decode(FORMAT)
 
@@ -110,10 +190,15 @@ def handle_client(conn,addr):
     conn.close()
     print("disconnected client")
     
+# Starts Server:
+# the assignment requires only one connection but my code can work properly with multiple connections.
+# the method starts the server and read the database from file data.txt which should be in the same directory
+# database configuration are provided at the top of this file
 
 def start():
     server.listen()
     print("server is listening on {}".format(SERVER))
+    readDB()
     while True:
         conn,addr = server.accept()
         thread = threading.Thread(target=handle_client,args=(conn,addr))
